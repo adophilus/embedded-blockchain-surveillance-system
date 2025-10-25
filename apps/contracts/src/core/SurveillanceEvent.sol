a6dd68ad0236c845e54a2faf7fc04c3eccff46a2
Adophilus <uchenna19of@gmail.com>
2025-10-25 14:34:44 +0100

    chore: updated struct

--- apps/contracts/src/core/SurveillanceEvent.sol
+++ apps/contracts/src/core/SurveillanceEvent.sol
@@ -1,7 +1,14 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24;
 
+struct Detection {
+    string criminal_profile_id;
+}
+
 struct SurveillanceEvent {
-    uint timestamp;
-    bool detected;
+    string id;
+    Detection[] detections;
+    string device_id;
+    string session_id;
+    uint created_at;
 }

be2a239a1bf5e55aef6163a9ac32a29ce4fac108
Adophilus <uchenna19of@gmail.com>
2025-10-25 18:00:52 +0100

    chore: updated vite config

--- apps/webapp/vite.config.ts
+++ apps/webapp/vite.config.ts
@@ -6,17 +6,20 @@ import path from "node:path";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    tanstackRouter({
-      // target: "react",
-      autoCodeSplitting: true,
-    }),
-    react(),
-    tailwindcss(),
-  ],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
+	build: {
+		outDir: "./build",
+	},
+	plugins: [
+		tanstackRouter({
+			// target: "react",
+			autoCodeSplitting: true,
+		}),
+		react(),
+		tailwindcss(),
+	],
+	resolve: {
+		alias: {
+			"@": path.resolve(__dirname, "./src"),
+		},
+	},
 });

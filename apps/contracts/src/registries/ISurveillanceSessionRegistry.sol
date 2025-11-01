d1dfbc533e283bea9bc24e8f1646b4219d1db1d8
Adophilus <uchenna19of@gmail.com>
2025-11-01 15:09:08 +0100

    chore: removed import

--- apps/contracts/src/registries/ISurveillanceSessionRegistry.sol
+++ apps/contracts/src/registries/ISurveillanceSessionRegistry.sol
@@ -1,8 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24;
 
-import {SurveillanceSession} from "../core/SurveillanceSession.sol";
-
 interface ISurveillanceSessionRegistry {
     event SessionCreated(string id, address addr);
     event SessionStatusUpdated(string id, SessionStatus status);

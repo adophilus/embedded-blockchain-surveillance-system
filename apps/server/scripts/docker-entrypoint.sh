c99d8b24757f5bb3c4b115c3fb304cd9efc05e77
Adophilus <uchenna19of@gmail.com>
2025-11-04 20:58:14 +0100

    chore: updated docker entrypoint

--- apps/server/scripts/docker-entrypoint.sh
+++ apps/server/scripts/docker-entrypoint.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 export INFISICAL_TOKEN=$(infisical login --method=universal-auth --client-id=$INFISICAL_CLIENT_ID --client-secret=$INFISICAL_CLIENT_SECRET --silent --plain)
-infisical run --env production -- pnpm --filter @embedded-blockchain-surveillance-system/server start
+infisical run --projectId $INFISICAL_PROJECT_ID --env production -- pnpm --filter @embedded-blockchain-surveillance-system/server start
